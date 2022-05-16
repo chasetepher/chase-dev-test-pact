24d47e6cb6007e6848d233097e666d085767d43e
Chase Riggs <criggstx@gmail.com>
2022-05-16 18:04:53 -0500

    carousel is present, functionality not ideal yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 // import logo from './logo.svg'
 import './App.css'
+// import AppContext from './Context';
+import Carousel from './Carousel.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    // <AppContext.Provider >
+      <div className="App">
       <div className="App-header">
       <header className="headline-text">
-        <p>OCCASIONALLY, WE OFFER PERSPECTIVES ON THE DIRECT-TO-CONSUMER LANDSCAPE, RETAIL INNOVATIONS, HAPPENINGS AROUND PACT AND THE ODD CREATIVE EXPERIMENT.</p>
+        <p>OCCASIONALLY, WE OFFER 2PERSPECTIVES ON THE DIRECT-TO-CONSUMER LANDSCAPE, RETAIL INNOVATIONS, HAPPENINGS AROUND PACT AND THE ODD CREATIVE EXPERIMENT.</p>
         <p>
         </p>
       </header>
     </div>
+    <div className='carousel-holder'>
+      <Carousel/>
     </div>
+    </div>
+    // </AppContext.Provider>
   )
 }
 

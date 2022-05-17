0e403bd62c15c9040ef2a7b7d4cd3a4c3509f930
Chase Riggs <criggstx@gmail.com>
2022-05-17 12:06:41 -0500

    basic layout done, needs further styling

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,16 @@ function App() {
     <div className='carousel-holder'>
       <Carousel/>
     </div>
+    <div className='footer-blurb'>
+      <p className='intersection'>THE INTERSECTION OF DESIGN, TECHNOLOGY AND BUSINESS.</p>
+      <p className='about-blurb'>Pact takes a unique, team-like, and holistic approach to building direct-to-consumer businesses: we do more across a variety of touchpoints for
+      fewer clients so that we can be accountable and flexible to all the road bumps that inherently come with building something new.</p>
+
+          <button className='work-button' type="button" onClick={() => alert('woah')}>
+            START YOUR PROJECT WITH PACT
+          </button>
+
+    </div>
     </div>
     // </AppContext.Provider>
   )
